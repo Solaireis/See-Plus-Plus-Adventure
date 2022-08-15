cf3a836306f9a0ef82a0cee750a6688a3ab479e9
Eden <64953425+Solaireis@users.noreply.github.com>
2022-08-15 14:26:13 +0800

    testing stuff

---
+++ test.cpp
@@ -0,0 +1,14 @@
+#include <iostream>
+
+using namespace std;
+
+void testfat()
+{
+    cout << "Hello World! I do love the universe, and I am very fat";
+}
+
+int main()
+{
+    testfat();
+    return 0;
+}
