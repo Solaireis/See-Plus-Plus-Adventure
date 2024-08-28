f8ea5a044814124ede32e389069befd3102c15e4
Eden <64953425+Solaireis@users.noreply.github.com>
2024-08-29 00:27:47 +0800

    cleanup

---
+++ hackerrank/solveMeFirst.cpp
@@ -0,0 +1,20 @@
+#include <cmath>
+#include <cstdio>
+#include <vector>
+#include <iostream>
+#include <algorithm>
+using namespace std;
+
+
+int main() {
+    /* Enter your code here. Read input from STDIN. Print output to STDOUT */ 
+    int a, b; /* initialise two integer variable */
+    
+    cin >> a >> b; /* assign inputs */
+    
+    int solveMeFirst = a + b; /* summation */
+    
+    cout << solveMeFirst; /* prints soln */
+    
+    return 0;
+}
